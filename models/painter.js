9c6c61e68f5893a547c4cadf326329ed533756ce
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-16 17:49:01 +0100

    Added method to check how much paint the Painter has in stock and a method to check if the painter has enough for a given room

--- models/painter.js
+++ models/painter.js
@@ -10,4 +10,22 @@ Painter.prototype.addPaint = function(paint) {
     this.paintStock.push(paint);
 };
 
+Painter.prototype.calculatePaintInStock = function() {
+    let litresOfPaint = 0
+    for (var paint of this.paintStock) {
+        litresOfPaint += paint.litres
+    };
+    return litresOfPaint;
+}; 
+
+Painter.prototype.checkEnoughPaint = function(area) {
+    if (this.calculatePaintInStock >= area) {
+        return true
+    } else {
+        return false
+    };
+};
+
+
+
 module.exports = Painter
--- models/specs/painter_specs.js
+++ models/specs/painter_specs.js
@@ -5,6 +5,8 @@ const Painter = require("../painter");
 
 describe("Painter", function() {
     let painter
+    const paint = new Paint(10);
+    const room = new Room(42);
 
     beforeEach(function() {
         painter = new Painter()
@@ -19,10 +21,22 @@ describe("Painter", function() {
 
     describe("Methods", function() {
         it("should be able to add can to stock", function() {
-            const paint = new Paint(10);
             painter.addPaint(paint);
             const actual = painter.numberOfPaintStock();
             assert.strictEqual(actual, 1);
+        });
+
+        it("should be able to calculate total number of litres", function() {
+            const morePaint = new Paint(5);
+            painter.addPaint(paint);
+            painter.addPaint(morePaint);
+            const actual = painter.calculatePaintInStock()
+            assert.strictEqual(actual, 15);
+        });
+
+        it("should be able to check if there's enough paint for a room", function(){
+            const actual = painter.checkEnoughPaint(room.area);
+            assert.strictEqual(actual, false);
         });
     });
     
